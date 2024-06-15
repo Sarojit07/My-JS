1a091476affe5a5ffa6f87a99d1ee5e81196e7fb
Sarojit07 <108582002+Sarojit07@users.noreply.github.com>
2024-06-15 11:53:46 +0000

    container config done

---
+++ .devcontainer/01_basics/01_varriable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "sarojit@gmai.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "sd@sd.com"
+accountPassword = "23652"
+accountCity = "Asansole"
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
